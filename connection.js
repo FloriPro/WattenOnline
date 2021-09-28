2de9b0e029a51ab52f0b16ba9881fc81ba274e58
FloriPro <floriprolohner@gmail.com>
2021-09-28 17:15:10 +0200

    updated

--- connection.js
+++ connection.js
@@ -28,52 +28,133 @@ if (getCookie("serverIpAdress") == null) {
   document.cookie = "serverIpAdress=" + prompt("Ip adress:") + "; expires=Fri, 31 Dec 9999 23:59:59 GMT";
 }
 
+var type = "none";
 var ws;
+var HauptFar;
+var HauptSla;
+var posYou;
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
   ws = new WebSocket("ws://" + getCookie("serverIpAdress") + ":8000/");
 
 
   var cards = [];
 
-  ws.onopen = function(event) {
+  ws.onopen = function (event) {
     console.log("ok");
     document.getElementById("position").style.display = "inline-table";
     document.getElementById("startingConnection").style.display = "none";
     document.getElementById("NeueIP").style.display = "none";
   }
 
-  ws.onerror = function(event) {
+  ws.onerror = function (event) {
     if (document.getElementById("startingConnection").style.display != "none") {
-
+      console.log(event);
       document.getElementById("startingConnection").textContent = "Verbindung fehlgeschlagen";
     }
 
   }
 
-  ws.onmessage = function(event) {
+  ws.onmessage = function (event) {
     console.log(event.data)
     if (event.data.substring(0, 2) == "PL") {
       p = event.data.substring(2, 0);
 
       document.getElementById("waitingPlayer").innerHTML = "Vorhandene Spieler: " + event.data.substring(2, 3)
     }
-    if (event.data.substring(0, 3) == "Sel") {
+    if (event.data.substring(0, 7) == "YourSel") {
+      type = "YourSel"
+      document.getElementById("Msg").textContent = "Lege eine Karte:";
+      document.getElementById("Msg").style.display = "unset";
+    }
+    if (event.data.substring(0, 3) == "IT:") {
+      event.data.substring(3, 999).split("#").forEach(function (item, index, array) {
+        cards.push(item)
+        document.getElementById("yourCards").innerHTML += '<a href="#" width="25%" height="25%"><img alt="' + item + '" src="/deck/own/' + item + '.png" id="' + item + '" onclick="cardSelect(\'' + item + '\')"></a>';
+      });
+      document.getElementById("SticheAnzahl").style.display = "unset";
+    }
+    if (event.data.substring(0, 2) == "FR") {
+      event.data.substring(3, 999).split("#").forEach(function (item, index, array) {
+        document.getElementById(item).style.display = "unset"
+      });
+    }
+    if (event.data.substring(0, 6) == "WaiOnP") {
+    }
+    if (event.data.substring(0, 5) == "clear") {
       document.getElementById("waiting").style.display = "none";
       document.getElementById("game").style.display = "block";
     }
-    if (event.data.substring(0, 3) == "IT:") {
-      event.data.substring(3, 999).split("#").forEach(function(item, index, array) {
-        cards.push(item)
-        document.getElementById("yourCards").innerHTML += '<img alt="' + item + '" src="/deck/own/' + item + '.png" width="25%" height="25%">';
-      });
+    if (event.data.substring(0, 7) == "waitSla") {
+      document.getElementById("info").style.display = "unset";
+      document.getElementById("info").textContent = "Warte auf selection vom Schlag";
     }
+    if (event.data.substring(0, 7) == "SelSla") {
+      type = "selSla"
+      document.getElementById("info").style.display = "unset";
+      document.getElementById("info").textContent = "Bitte Schlag auswählen";
+    }
+    if (event.data.substring(0, 7) == "waitFar") {
+      document.getElementById("info").style.display = "unset";
+      document.getElementById("info").textContent = "Warte auf selection von Farbe -- Schlag: " + HauptSla;
+    }
+    if (event.data.substring(0, 7) == "SelFar") {
+      type = "selFar"
+      document.getElementById("info").style.display = "unset";
+      document.getElementById("info").textContent = "Bitte Farbe auswählen -- Schlag: " + HauptSla;
+    }
+    if (event.data.substring(0, 6) == "Stapel") {
+      document.getElementById("layedCards").innerHTML = "";
+      if (event.data.substring(6, 999) != "") {
+        event.data.substring(6, 999).split("#").forEach(function (item, index, array) {
+          cards.push(item)
+          document.getElementById("layedCards").innerHTML += '<img src="/deck/own/' + item + '.png" width="30%" height="30%" style="margin-right: -20%;" alt="' + item + '">';
+        });
+      }
+    }
+    if (event.data.substring(0, 12) == "farSelected_") {
+      console.log("selected Farbe: " + event.data.substring(12, 13));
+      HauptFar = event.data.substring(12, 13);
+      document.getElementById("Hauptschlagt").innerHTML += '<img width="20%" height="20%" alt="' + HauptFar + "_" + HauptSla + '" src="/deck/own/' + HauptFar + "_" + HauptSla + '.png" style="position:fixed;margin-left: 80%;">';
+      document.getElementById("info").style.display = "none";
+    }
+    if (event.data.substring(0, 12) == "slaSelected_") {
+      console.log("selected Schlag: " + event.data.substring(12, 14));
+      HauptSla = event.data.substring(12, 14);
+    }
+    if (event.data.substring(0, 6) == "Stiche") {
+      document.getElementById("SticheOL").textContent = event.data.substring(6, 999).split("#")[0] + " Stich(e)"
+      document.getElementById("SticheOR").textContent = event.data.substring(6, 999).split("#")[1] + " Stich(e)"
+      document.getElementById("SticheUR").textContent = event.data.substring(6, 999).split("#")[2] + " Stich(e)"
+      document.getElementById("SticheUL").textContent = event.data.substring(6, 999).split("#")[3] + " Stich(e)"
+    }
+
   };
 
 });
 
 function init(pos) {
+  posYou = pos;
+  document.getElementById("Stiche" + pos).style.backgroundColor = "crimson";
   ws.send(pos);
   document.getElementById("position").style.display = "none";
   document.getElementById("waiting").style.display = "block";
+}
+
+function cardSelect(cardId) {
+  console.log(cardId);
+  if (type == "selSla") {
+    ws.send(cardId)
+    type = ""
+  }
+  if (type == "selFar") {
+    ws.send(cardId)
+    type = ""
+  }
+  if (type == "YourSel") {
+    document.getElementById(cardId).remove();
+    document.getElementById("Msg").style.display = "none";
+    ws.send(cardId)
+    type = ""
+  }
 }
--- server.py
+++ server.py
@@ -1,80 +1,249 @@
 import asyncio
+from asyncio.tasks import sleep
 import random
 import websockets
 
+
 class Card:
     def __init__(self) -> None:
-        #gnerate cards
+        # gnerate cards
         #Eichel, Grass, Herz, Schellen
-        Farben=["E","G","H","S"]
-        Zahlen=["7","8","9","10","U","O","K","A"]
-        Deck=[]
+        Farben = ["E", "G", "H", "S"]
+        Zahlen = ["7", "8", "9", "10", "U", "O", "K", "A"]
+        Deck = []
         for F in Farben:
             for Z in Zahlen:
-                Deck+=[f"{F}_{Z}"]
+                Deck += [f"{F}_{Z}"]
         random.shuffle(Deck)
 
-        #austeilen
-        self.players={0:[],1:[],2:[],3:[]}
+        # austeilen
+        self.players = {0: [], 1: [], 2: [], 3: []}
         for i in range(4):
             for x in range(3):
-                self.players[i]+=[Deck[-1]]
+                self.players[i] += [Deck[-1]]
                 Deck.pop(-1)
         for i in range(4):
-            for x in range(2    ):
-                self.players[i]+=[Deck[-1]]
+            for x in range(2):
+                self.players[i] += [Deck[-1]]
                 Deck.pop(-1)
-        i=0
+        i = 0
         for _ in self.players:
-            self.players[i]=sorted(self.players[i])
-            i+=1
+            self.players[i] = sorted(self.players[i])
+            i += 1
 
         print(self.players)
 
+
 def getPlayerId(name):
-    i=0
+    i = 0
     for x in line:
-        if x ==name:
+        if x == name:
             return i
-        i+=1
+        i += 1
+def getPoints(HsFar,HsSla):
+    F=["E","G","H","S"]
+    Z=["A","10","K","O","U","9","8","7"]
 
-Card=Card()
+    goodBad=["H_K","S_7","E_7",f"{HsFar}_{HsSla}"]
+    for x in F:
+        if f"{x}_{HsSla}" not in goodBad:
+            goodBad+=[f"{x}_{HsSla}"]
+    for x in Z:
+        if f"{HsFar}_{x}" not in goodBad:
+            goodBad+=[f"{HsFar}_{x}"]
+    for x in F:
+        for y in Z:
+            if f"{x}_{y}" not in goodBad:
+                goodBad+=[f"{x}_{y}"]
+    
+    goodBad.reverse()
+    return goodBad
+def evaluateWinner(Stapel,HsFar,HsSla):
+    # generate best to worst card
+    goodBad=getPoints(HsFar,HsSla)
+    Stuff={}
+    for key,value in Stapel.items():
+        Stuff[goodBad.index(key)]=[key,value]
+    print(Stuff)
+    m=0
+    best=[]
+    for key,value in Stuff.items():
+        if key>m:
+            best=value
+            m=key
+    return best
 
-line=["OL","OR","UR","UL"]
-activePlayer=0
-players=0
-Stiche=[0,0,0,0]
-waiting=-1
-stapel=[]
+#Test
+#evaluateWinner({"G_A":1,"E_7":2,"G_7":3,"H_K":0},"G","A")
+
+Card = Card()
+
+line = ["OL", "OR", "UR", "UL"]
+usedPos = [False, False, False, False]
+activePlayer = 1
+players = 0
+Stiche = [0, 0, 0, 0]
+waiting = -1
+playerRound = 0
+stapel = {}
+
+Sla = ""
+Far = ""
+w1 = True
+w2 = True
+
+
+def a1(var):
+    if var != 3:
+        return var+1
+    else:
+        return 0
+
+
+def d(pos):
+    global usedPos
+    global players
+    players -= 1
+    print(f"{pos} disconnected!")
+    usedPos[pos] = False
+
 
 async def time(websocket, path):
     global players
     global waiting
     global stapel
-    pos = getPlayerId(await websocket.recv())
+    global activePlayer
+    global w1
+    global w2
+    global Sla
+    global Far
+
+    # send free spaces
+    i = 0
+    ok = []
+    for x in usedPos:
+        if x == False:
+            ok += [line[i]]
+        i += 1
+    await websocket.send(f"FR_{'#'.join(ok)}")
+
+    # wait for position selection
+    try:
+        pos = getPlayerId(await websocket.recv())
+        usedPos[pos] = True
+    except:
+        return
     print(Card.players[pos])
 
     await websocket.send("IT:"+"#".join(Card.players[pos]))
 
-    players+=1
-    while players<4:
-        await websocket.send(f"PL{players}")
+    # waiting for players to join
+    players += 1
+    while players < 4:
+        try:
+            await websocket.send(f"PL{players}")
+        except:
+            d(pos)
+            return
         await asyncio.sleep(1)
-    print(f"{pos}: ok")
-    while True:
-        if activePlayer==pos:
-            print(f"its {pos}")
-            waiting=pos
-            await websocket.send("Sel")
-            data = await websocket.recv()
-            stapel+=[data]
-        else:
-            while waiting!=-1:
-                await websocket.send(f"WaiOnP{waiting}")
-                await asyncio.sleep(0.25)
-            await websocket.send(f"S_{'#'.join(stapel)}")
-        await asyncio.sleep(0.1)
 
+    try:
+        await websocket.send("clear")
+    except:
+        d(pos)
+        return
+    ##############################
+    # selecting Schlag
+    if pos == a1(playerRound):
+        # Schlag
+        await websocket.send("SelSla")
+        Sla = await websocket.recv()
+        Sla = Sla[2:]
+        print("Schlag: "+Sla)
+        w1 = False
+
+    # wait for Schlag select
+    else:
+        while w1:
+            await websocket.send("waitSla")
+            await asyncio.sleep(0.5)
+    await websocket.send(f"slaSelected_{Sla}")
+
+    # selecting Farbe
+    if pos == playerRound:
+        # Farbe
+        await websocket.send("SelFar")
+        Far = await websocket.recv()
+        Far = Far[0]
+        w2 = False
+
+        a=getPoints(Far,Sla)
+        b=[]
+        for x in a:
+            b+=[x.replace("E","Eichel").replace("S","Schellen").replace("H","Herz").replace("G","Graß")]
+        print(b)
+
+    # wait for Farbe select
+    else:
+        while w2:
+            await websocket.send("waitFar")
+            await asyncio.sleep(0.5)
+    await websocket.send(f"farSelected_{Far}")
+    ##############################
+
+    print(f"{pos}: ok")
+    await websocket.send(f"Stapel{'#'.join(stapel)}")
+    while True:
+        if activePlayer == pos:
+
+            print(f"its {pos}")
+            waiting = pos
+            try:
+                await websocket.send("YourSel")
+            except:
+                d(pos)
+                return
+            try:
+                data = await websocket.recv()
+            except:
+                d(pos)
+                return
+            stapel[data] = pos
+            activePlayer=a1(activePlayer)
+            waiting=-1
+            if len(stapel)>=4:
+                await asyncio.sleep(0.5)
+                best=evaluateWinner(stapel,Far,Sla)
+                Stiche[best[1]]+=1
+                i=[]
+                for x in Stiche:
+                    i+=[str(x)]
+                activePlayer=best[1]
+                await websocket.send(f"Stiche{'#'.join(i)}")
+                stapel={}
+                await websocket.send(f"Stapel{'#'.join(stapel)}")
+                if Stiche[0]+Stiche[2]>=3:pass
+                if Stiche[1]+Stiche[3]>=3:pass
+            
+        else:
+            try:
+                while waiting != -1:
+                    await websocket.send(f"WaiOnP{waiting}")
+                    await asyncio.sleep(0.25)
+                await websocket.send(f"Stapel{'#'.join(stapel)}")
+            except:
+                d(pos)
+                return
+            
+
+            if len(stapel)>=4:
+                await asyncio.sleep(0.6)
+                i=[]
+                for x in Stiche:
+                    i+=[str(x)]
+                await websocket.send(f"Stiche{'#'.join(i)}")
+                await websocket.send(f"Stapel{'#'.join(stapel)}")
+        await asyncio.sleep(0.1)
 
 
 start_server = websockets.serve(time, '', 8000)
